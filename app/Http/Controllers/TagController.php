cda6f79a6502d2d263b862e8c6e170c5cad5d3cc
Misa Cvetkovic <misa.cvetkovic01@gmail.com>
2016-08-09 14:48:07 +0200

    This is a new commit for part 38 of the laravel tutorial

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -62,7 +62,9 @@ class TagController extends Controller
      */
     public function show($id)
     {
-        //
+        $tag = Tag::find($id);
+
+        return view('tags.show')->withTag($tag);
     }
 
     /**
@@ -73,7 +75,9 @@ class TagController extends Controller
      */
     public function edit($id)
     {
-        //
+        $tag = Tag::find($id);
+
+        return view('tags.edit')->withTag($tag);
     }
 
     /**
@@ -85,7 +89,16 @@ class TagController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $tag = Tag::find($id);
+
+        $this->validate($request, ['name' => 'required|max:255']);
+
+        $tag->name = $request->name;
+        $tag->save();
+
+        Session::flash('success', 'Successfully saved your new tag!');
+
+        return redirect()->route('tags.show', $tag->id);
     }
 
     /**
